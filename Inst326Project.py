4f559277e39bc701eb80a6a1035ddd9648377070
Neil Vu <neilvu80@gmail.com>
2025-11-14 18:23:12 -0500

    Refactor pizza order and assembly functions
    Refactor customer order function to handle pizza selection and improve error handling. Add methods for pizza assembly timing and topping selection limits.

--- Inst326Project.py
+++ Inst326Project.py
@@ -2,18 +2,86 @@ import sys
 import time
 
 
+pizza = []
+
+# Problem A: Creating Customer Orders based on the type of pizzas
+def customer_order(self, pizza):
+    """Identifies the customer's order through searching different types of pizzas they wished to order
+    
+    Args:
+        self = reflects from lists of pizzas that each customer had specifcially ordered
+        pizza = a json file list that shows the different kinds of pizza
+    
+    Returns:
+        size = selected pizza size
+        meat = selected type of meat (if desired) 
+        veg =  selected type of veggies (if desired) 
+
+    Side Effects:
+        Modifes the different orders for each customer type (by each round of the game)
+
+    """
+    
+    #load json file 
+    pizza = {self}
+    
+    #group following files
+    size = group(size)
+    meat = group(meat)
+    veggies = group(veggies)
+    
+    
+    #find the specific pizza using regular expression
+    ## a). based on size 
+    for spec_types in pizza:
+        for size_search in pizza: 
+            size_search = re.search(r"\b(P<size>)")
+            #match the size
+            try:
+                size_search == size
+                #return with the size
+                size 
+            #otherwise return None
+            except:
+                NameError = "Incorrect Size"
+        
+        ## b). based on any meat type (if not none)
+        while True:
+            for meat_search in pizza: 
+                meat_search = re.search(r"\b(P<meat>)")
+                #match the size
+                if meat_search == meat:
+                    #return with the meat
+                    order = meat 
+                #otherwise return None
+                else:
+                   "Try Again"
+        ## c). based on any veggies (if not none)
+        while True:
+            for veg_search in pizza: 
+                veg_search = re.search(r"\b(P<veggies>)")
+                #match the size
+                if veg_search == veg:
+                    #return with the veggies
+                    order = veg 
+                #otherwise return None
+                else:
+                    "Try Again"
+    
+    #do a couple of rounds as well for each customer
+    spec_types += 1
+    #return with the description of the order list the given customer ordered by dictionary
+    return Cus_Order == (size, meat, veg)
 
 
 
 
 
 
+##### PROBLEM B: PIZZA ASSEMBLY
 
 
-
-
-
-# Problem B.) assembling pizza with a timer countdown
+# Problem B.1) assembling pizza with a timer countdown
 def time_countdown():
     """Time how long it takes to assemble the pizza and check if under 3 min
     
@@ -46,5 +114,40 @@ def time_countdown():
     else:
         print("Ran out of time!")
         return False
+
+
+#Problem B)2. Max Amount of Selection of Toppings
+
+#### 2). This section asks for how many max amount opping selections that each player can use for the game (MOST CAN BE ESTABLISHED USING SELF CLAUSE)
+        
+def pizza_amount_selection(self, pizza_type):
+    """Sets the max amount of toppings in the pizza assembly
+    
+    Args: 
+        pizza_type = connects from the assembly line and in prev. functions on type of pizzas
+        
+    Return:
+        self.ingreidents = the ingridents represented
+        apply_ingr = the ingridents applied
+
+    """
+    #make a dict that shows the lists
+    self.pizza_type = {self.name_pizza:self.ingridents}
+    pizza_type = self.pizza_type
+    #Make a for loop that decides how many ingredients that they need 
+    for amt_ing in pizza_type:
+        #Filter based on the ingredients and the quantity of it
+        self.ingridents, amt_ing
+        #Choose how many toppings we need for max (one says three), then apply it
+        #[EXP1 if COND1 else COND2]
+        [apply_ingr if amt_ing <= 3 else f"Must meet 3 toppings max"]
+        #Apply to the following 
+        return self.ingridents, apply_ingr
+
+
+
+
+
+## PROBLEM C: Delivery
     
     
